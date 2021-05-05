eb63cc0319d0f1cde46108cb252c0a4abe34f191
techchrism <chris@techchrism.me>
2021-05-05 14:26:44 -0700

    display virtual scroller of uri data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
     <v-app>
-        <v-app-bar app color="primary" dark>
+        <v-navigation-drawer
+            v-if="ready"
+            v-model="drawer"
+            :clipped="$vuetify.breakpoint.lgAndUp"
+            :width="500"
+            ref="nav"
+            app
+        >
+            <events-list :height="navHeight"/>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="primary" dark :clipped-left="$vuetify.breakpoint.lgAndUp">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="ready"></v-app-bar-nav-icon>
             <v-toolbar-title class="headline">
                 <span class="font-weight-light">Valorant Websocket Log Viewer</span>
                 <v-btn icon href="https://github.com/techchrism/valorant-websocket-log-viewer">
@@ -12,26 +24,54 @@
         </v-app-bar>
 
         <v-main>
-            <file-dropper/>
+            <template v-if="!ready">
+                <!-- TODO add explanation -->
+                <file-dropper/>
+            </template>
+            <template v-else>
+                <h4>Info</h4>
+            </template>
         </v-main>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 import DarkButton from '@/components/DarkButton';
 import FileDropper from '@/components/FileDropper';
+import EventsList from '@/components/EventsList';
 
 export default {
     name: 'App',
     components: {
+        EventsList,
         FileDropper,
-        DarkButton,
-        HelloWorld
+        DarkButton
+    },
+    computed: {
+        ready()
+        {
+            return this.$store.state.lockData !== null;
+        }
     },
     data: () => ({
-        //
-    })
+        drawer: null,
+        navHeight: 0
+    }),
+    mounted()
+    {
+        this.navHeight = window.innerHeight - 64;
+        window.addEventListener('resize', this.resizeEvent);
+    },
+    destroyed()
+    {
+        window.removeEventListener('resize', this.resizeEvent);
+    },
+    methods: {
+        resizeEvent()
+        {
+            this.navHeight = window.innerHeight - 64;
+        }
+    }
 };
 </script>
 
---
+++ src/components/EventsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <v-virtual-scroll :items="$store.state.events" :item-height="50" :height="height">
+            <template v-slot:default="{item}">
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{item.uri}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
+        </v-virtual-scroll>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EventsList',
+    props: {
+        height: Number
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
